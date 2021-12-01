6fa0e7e3ba8f27bb3bc4c6123de1c2e39750a3e7
Umar Ahmed <umar.ahmed1998@gmail.com>
2021-12-01 02:05:56 +0000

    wip: update example

--- example/src/main.jsx
+++ example/src/main.jsx
@@ -1,14 +1,28 @@
-import React from "react";
-import { createRoot } from "@react-three/fiber";
+import React, { useRef } from "react";
+import { createRoot, useFrame } from "@react-three/fiber";
 import "./index.css";
+
+function Box(props) {
+  const ref = useRef();
+  useFrame(() => (ref.current.rotation.y = ref.current.rotation.x += 0.01));
+  return (
+    <mesh {...props} ref={ref}>
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color="orange" />
+    </mesh>
+  );
+}
+
+function App() {
+  return (
+    <>
+      <ambientLight />
+      <pointLight position={[10, 10, 10]} />
+      <Box position={[0, 0, 0]} />
+    </>
+  );
+}
 
 const canvasNode = document.getElementById("canvas");
 
-
-createRoot(canvasNode).render(
-  <mesh>
-    <boxGeometry />
-    <meshBasicMaterial color='orange' />
-  </mesh>,
-  canvasNode
-);
+createRoot(canvasNode).render(<App />, canvasNode);
